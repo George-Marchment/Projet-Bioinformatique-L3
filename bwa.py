98e5b2a5243aacfaf820b65d6c9f3f89450ceda6
ClemenceS <clemence.22.sebe@gmail.com>
2021-01-29 22:14:57 +0100

    ajout samtools

--- bwa.py
+++ bwa.py
@@ -7,11 +7,20 @@ import shutil
 import os
 import download
 import variables as v
+import matplotlib.pyplot as plt
+import numpy as np
+
+def unzip(nomDossier, nomUnzip):
+	#unzip
+	with gzip.open(nomDossier, 'rb') as f_in:
+		with open(nomUnzip, 'wb') as f_out:
+			shutil.copyfileobj(f_in, f_out)
 
 def mainBWA(telechargement):
 	#recuperer tab
 	tabFichier = download.mainDownload(telechargement)
 	print(tabFichier)
+	tabFig = []
 
 	print("DEBUT SCRIPT BWA")
 
@@ -28,10 +37,48 @@ def mainBWA(telechargement):
 	#cmd = "./bwa mem " + geneRef + " " + "../Donnees/" + tabFichier[i] + ".fastq.gz"  + " | gzip -3 > " + nomZip
  
 	for i in range (len(tabFichier)):
+		os.chdir(v.adresseBwa)
 		print("----------------------BOUCLE----------------------", i+1 , " sur " , len(tabFichier)) 
 		nomZip = v.zipSam + tabFichier[i] + ".sam.gz"
+		nomUnzip = v.zipSam + tabFichier[i] + ".sam"
 		cmd = "./bwa mem " + v.geneRef + " " + v.samRef + tabFichier[i] + ".fastq.gz"  + " | gzip -3 > " + nomZip
 		os.system(cmd)
-
+		
+		print("Convertion du fichier : ", nomZip, " en un fichier .bam")
+		unzip(nomZip, nomUnzip)
+		os.remove(nomZip)
+		fichierBam = v.bamRef + tabFichier[i] + ".bam"
+		cmd = "samtools view -bS " + nomUnzip + " > " + fichierBam 
+		os.system(cmd)
+		
+		print("Samtools flagstat + creation fichier txt")
+		flag = tabFichier[i] + ".txt"
+		cmd = "samtools flagstat " + fichierBam + " > " +  v.bamRef + flag
+		os.system(cmd)
+	
+		print("Ajout du pourcentage dans un tableau pour figure % qui mappe")
+		#Ajout donnee pour figure
+		os.chdir(v.fichTxt)
+		file = open(flag, "r")
+		for i in range (4):
+			line = file.readline()
+		ligne5 = file.readline()
+		l = ligne5.split()
+		cas = l[4]
+		cas = cas.split('(')
+		new = cas[1]
+		new = new.split("%")
+		num = new[0]
+		tabFig.append(num)
+		file.close()
+	
+	os.chdir(v.simple)
+	print("Creation figure")
+	fig,ax = plt.subplots()
+	ax.plot(tabFig)
+	ax.set_title("% de donnees qui mappe")
+	plt.savefig('image.png')
+		
 	os.chdir(current_path)
 	print("FINI")
+	
--- variables.py
+++ variables.py
@@ -14,26 +14,35 @@ def initialize():
     global geneRef
     global samRef
     global zipSam
+    global bamRef
+    global fichTxt
+    global simple
     
     if utilisateur == "george":
         print("")
         print("L'utilsateur est George")
         print("")
+        simple = "/media/george/USB2GM/Projet_BioInformatique/"
         adresseTelechargement= "/media/george/USB2GM/Projet_BioInformatique/Données"
         adresseBwa= "/home/george/Bureau/bwa/"
         geneRef= "../../../../media/george/USB2GM/Projet_BioInformatique/Données/Genome/S288C_reference_sequence_R64-2-1_20150113.fsa"
         samRef= "../../../../media/george/USB2GM/Projet_BioInformatique/Données/"
         zipSam = samRef
+        bamRef = samRef
+        fichTxt = samRef
         
     elif utilisateur == "clemence":
         print("")
         print("L'utilsateur est Clémence")
         print("")
+        simple = "/home/clemence/L3/S6/Projet-Bioinformatique-L3/"
         adresseTelechargement= "/home/clemence/L3/S6/Projet-Bioinformatique-L3/Donnees/" 
         adresseBwa= "/home/clemence/L3/S6/Projet-Bioinformatique-L3/bwa/"
         geneRef= "../Donnees/Genome/S288C_reference_sequence_R64-2-1_20150113.fsa"
         samRef= "../Donnees/"
         zipSam =  "../Donnees/SamRef/"
+        bamRef = "../Donnees/FichierBam/"
+        fichTxt = "/home/clemence/L3/S6/Projet-Bioinformatique-L3/Donnees/FichierBam/"
         
     else:               
         os.exit()
